443648cf4ce8adb99bba42d11c59401bfa38b2ab
gmori1020 <120454123+gmori1020@users.noreply.github.com>
2023-10-03 10:58:59 +0900

    課題2-1の途中まで

---
+++ scripts/kadai02_1.js
@@ -0,0 +1,13 @@
+let i = true;
+let n = 1;
+if(typeof i){
+    console.log("trueは実行できる");
+} else {
+    console.log("falseは実行できない");
+}
+
+if(typeof n) {
+    console.log("1なら実行できる " + n);
+} else {
+    console.log("0なら実行できない " + n);
+}
