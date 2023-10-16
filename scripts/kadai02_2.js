504cd45be4844a2a46f871ae6be212696a6985db
gmori1020 <120454123+gmori1020@users.noreply.github.com>
2023-10-16 13:03:53 +0900

    #1 課題2完成

---
+++ scripts/kadai02_2.js
@@ -0,0 +1,24 @@
+console.log("-- kadai02_2.js --");
+
+let Score = 80;
+
+console.log("今期のあなたの点数は " + Score + " です。成績は")
+
+if(Score <= 59) {console.log("Fです。");}
+
+if(Score >= 60 && Score <= 69){
+    console.log("Dです。");
+}
+
+if(Score >= 70 && Score <= 79){
+    console.log("Cです。");
+}
+
+if(Score >= 80 && Score <= 89){
+    console.log("Bです。")
+}
+
+if(Score >= 90 && Score <= 100){
+  console.log("Aです。");
+}
+
