30c2b8b22503874c081d36df0207ac4a12e5d819
gmori1020 <120454123+gmori1020@users.noreply.github.com>
2023-10-16 15:47:48 +0900

    #1 kadai02_2.js Update

--- scripts/kadai02_2.js
+++ scripts/kadai02_2.js
@@ -1,24 +1,19 @@
 console.log("-- kadai02_2.js --");
 
-let Score = 80;
+let Score = 59;
 
 console.log("今期のあなたの点数は " + Score + " です。成績は")
 
-if(Score <= 59) {console.log("Fです。");}
-
-if(Score >= 60 && Score <= 69){
+if(Score <= 59) {
+    console.log("Fです。");
+} else if(Score <= 69){
     console.log("Dです。");
-}
-
-if(Score >= 70 && Score <= 79){
+} else if(Score <= 79){
     console.log("Cです。");
-}
-
-if(Score >= 80 && Score <= 89){
+} else if(Score <= 89){
     console.log("Bです。")
-}
-
-if(Score >= 90 && Score <= 100){
+} else if(Score <= 100){
   console.log("Aです。");
+} else {
+    console.log("500");
 }
-
